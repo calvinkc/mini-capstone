72541893ea0dcf065ad8b009d97dfa8f6f163a85
Calvin <mrcalvinkc@gmail.com>
2017-05-02 19:41:38 -0700

    Create a Product Model with Attribs

---
+++ db/migrate/20170503024057_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :brand
+      t.string :type
+      t.float :price
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
