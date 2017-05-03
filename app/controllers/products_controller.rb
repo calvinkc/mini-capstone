b55aec267d180e7132cc55287960820238e4b826
Calvin <mrcalvinkc@gmail.com>
2017-05-02 20:35:02 -0700

    Added route for a show all item page, along with method define, etc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
 
   def shopping_method
+      @item = Product.last
     render "shopping.html.erb"
+  end
+
+  def shopping_all
+    @items = Product.all
+    render "all.html.erb"
   end
 end
