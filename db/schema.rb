7591fdbf41cc36eeafe5f0f695ac04477265ffd7
Calvin <mrcalvinkc@gmail.com>
2017-05-02 20:18:40 -0700

    Corrected Model attribute TYPE to CATEGORY

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503024057) do
+ActiveRecord::Schema.define(version: 20170503030848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string   "brand"
-    t.string   "type"
+    t.string   "category"
     t.float    "price"
     t.string   "description"
     t.datetime "created_at",  null: false
