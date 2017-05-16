9a3d2c0527a4f60d5829d7ddf2862d2484edc154
Calvin <mrcalvinkc@gmail.com>
2017-05-15 20:57:50 -0700

    Removed the Image(URL) column, to prepare for Image model associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516023225) do
+ActiveRecord::Schema.define(version: 20170516035448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170516023225) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "image"
     t.integer  "supplier_id"
   end
 
