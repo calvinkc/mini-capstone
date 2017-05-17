bbe2a51712bca8242a17e1aed255d5de5dd56b2e
Calvin <mrcalvinkc@gmail.com>
2017-05-16 20:48:15 -0700

    Added sessions controller, as well as login/logout session hashes

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,14 @@ get "/products/:id/edit" => "products#edit"
 patch "/products/:id" => "products#update"
 delete "/products/:id" => "products#destroy"
 
- get "/products/:id/images/new" => "images#new"
- post "/products/:id/images" => "images#create"
+get "/products/:id/images/new" => "images#new"
+post "/products/:id/images" => "images#create"
 
 get "/signup" => "users#new"
 post "/users" => "users#create"
+
+get "/login" => "sessions#new"
+post "/login" => "sessions#create"
+get "/logout" => "sessions#destroy"
 
 end
