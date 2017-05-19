620d8f3a48b0590f4d4366f3dcba6e5567187f95
Calvin <mrcalvinkc@gmail.com>
2017-05-18 21:02:32 -0700

    Removed the category column from products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518022459) do
+ActiveRecord::Schema.define(version: 20170519040120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20170518022459) do
 
   create_table "products", force: :cascade do |t|
     t.string   "brand"
-    t.string   "category"
     t.integer  "price"
     t.text     "description"
     t.datetime "created_at",  null: false
