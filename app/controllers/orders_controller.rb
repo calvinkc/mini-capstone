214d472b0da3c1c197b951da71c22da77221e268
Calvin <mrcalvinkc@gmail.com>
2017-06-01 20:50:36 -0700

    Updated orders controllers create and update methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,13 @@ class OrdersController < ApplicationController
 
   before_action :authenticate_user!
   
-  # def new
-  #   render "/products#"
-  # end
-
   def show
     @order = Order.find_by(id: params[:id]) 
   end
 
   def create
     product_id = params[:product_id]
+    quantity = params[:quantity].to_i
     product = Product.find_by(id: product_id)
     calculated_subtotal = product.price.to_i * quantity
     calculated_tax = calculated_subtotal * 0.09
@@ -19,38 +16,43 @@ class OrdersController < ApplicationController
 
     order = Order.create(
       user_id: current_user.id,
-      product_id: params[:product_id],
+      product_id: product_id,
+      quantity: quantity,
       subtotal: calculated_subtotal,
       tax: calculated_tax,
-      total: calculated_total,
-      completed: false
+      total: calculated_total
       )
     flash[:success] = "Order successfully created!"
     redirect_to "/orders/#{order.id}"
   end
 
   def update
-     order_id = params[:order_id]
-     calculated_subtotal = 0
-     cart = CartedProduct.where(order_id: order_id)
-     cart.each do |item|
-       calculated_subtotal += item.product.price
-     end
-     calculated_subtotal = cart.sum("price")
-     calculated_tax = calculated_subtotal * 0.09
-     calculated_total = calculated_tax + calculated_subtotal
+   #pull order from DB
+   order = Order.find_by(id: params[:id])
+   #mark order as complete
+   order.completed = true 
+   #initialize subtotal
+   calculated_subtotal = 0
+   #loop: find price for each item in cart, multi. by qt, then add them together.
+   order.carted_products.each do |item|
+     calculated_subtotal += item.product.price * item.quantity
+   end
+   calculated_tax = calculated_subtotal * 0.09
+   # update the order
+   order.subtotal = calculated_subtotal
+   order.tax = calculated_tax
+   order.total = calculated_subtotal + calculated_tax
+   order.save
 
-     order = Order.find(order_id)
-     order.update(
-     subtotal: calculated_subtotal,
-     tax: calculated_tax,
-     total: calculated_total,
-     completed: true
-     )
+   # order = Order.find(order_id)
+   # order.update(
+   # subtotal: calculated_subtotal,
+   # tax: calculated_tax,
+   # total: calculated_total,
+   # completed: true
+   # )    This is just another way to update that order
 
-     flash[:info] = "Product Updated!"
-     redirect_to "/orders/#{order_id}"
-
+   redirect_to "/orders/#{order_id}"
   end 
 
 end
